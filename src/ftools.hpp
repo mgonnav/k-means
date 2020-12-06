07ee0b01f02a72305288480c339645aa023b8788
mgonnav <mgonzalesn17@outlook.com>
2020-12-05 22:41:50 -0500

    Added drag and zoom functionalities

--- src/ftools.hpp
+++ src/ftools.hpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <sstream>
 #include <SFML/Graphics.hpp>
+#include <algorithm>
 
 namespace ftools {
 
@@ -19,15 +20,40 @@ void readFromFile(std::string filename,
 
   float start_lon, start_lat;
   std::string reg;
+  float min_lon = 400.f;
+  float min_lat = 400.f;
+  float max_lon = 0.f;
+  float max_lat = 0.f;
   // int i = 0;
 
   // while (getline(points, reg) && i++ < 300) {
+  size_t i = 1;
   while (getline(points, reg)) {
     std::stringstream point(reg);
     point >> start_lon >> start_lat;
+    min_lon = std::min(min_lon, start_lon);
+    min_lat = std::min(min_lat, start_lat);
+    max_lon = std::max(max_lon, start_lon);
+    max_lat = std::max(max_lat, start_lat);
+    if (start_lat < -90.f) 
+      std::cout << "min lat queals" << start_lat << " in line " << i << "\n";
+    if (start_lat > 90.f) 
+      std::cout << "max lat queals" << start_lat << " in line " << i << "\n";
+    if (start_lon < -180.f) 
+      std::cout << "min lon queals" << start_lon << " in line " << i << "\n";
+    if (start_lon > 180.f) 
+      std::cout << "min lon queals" << start_lon << " in line " << i << "\n";
+    ++i;
+
+
     data_points->append(sf::Vertex(sf::Vector2f(start_lon + 180, start_lat + 90),
                                       sf::Color::Red));
   }
+
+  std::cout << "Min lat: " << min_lat << "\n";
+  std::cout << "Max lat: " << max_lat << "\n";
+  std::cout << "Min lon: " << min_lon << "\n";
+  std::cout << "Max lon: " << max_lon << "\n";
 }
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 
 int main() {
   sf::VertexArray data_points(sf::Points);
+
   std::cout << "Loding data points..." << std::endl;
   ftools::readFromFile(DATA_FILENAME, &data_points);
   std::cout << "Loaded data points." << std::endl;
@@ -17,17 +18,59 @@ int main() {
 
   std::cout << "Calculating k-means with " << num_clusters << " clusters.\n";
 
-  sf::RenderWindow window(sf::VideoMode(400, 200), "K-means Clustering");
-  window.setFramerateLimit(24);
+  sf::RenderWindow window(sf::VideoMode(1280, 720), "K-means Clustering");
+  window.setFramerateLimit(5);
+
+  sf::View view(sf::FloatRect(58.f, 80.f, 260.f, 170.f));
+
+  bool mouse_moving = false;
+  sf::Vector2f oldPos;
 
   while (window.isOpen()) {
     sf::Event event;
 
     while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed)
-        window.close();
+      switch (event.type) {
+        case sf::Event::Closed:
+          window.close();
+          break;
+
+        case sf::Event::MouseWheelMoved:
+          view.zoom(1.f + event.mouseWheel.delta * -0.05f);
+          break;
+
+        case sf::Event::MouseButtonPressed:
+          if (event.mouseButton.button == 0) {
+            mouse_moving = true;
+            oldPos = window.mapPixelToCoords(sf::Vector2i(event.mouseButton.x,
+                                             event.mouseButton.y));
+          }
+          break;
+
+        case sf::Event::MouseButtonReleased:
+          if (event.mouseButton.button == 0)
+            mouse_moving = false;
+          break;
+
+        case sf::Event::MouseMoved:
+          {
+            if (!mouse_moving)
+              break;
+
+            const sf::Vector2f newPos = window.mapPixelToCoords(sf::Vector2i(event.mouseMove.x, event.mouseMove.y));
+            const sf::Vector2f deltaPos = oldPos - newPos;
+
+            view.setCenter(view.getCenter() + deltaPos);
+            window.setView(view);
+
+            oldPos = window.mapPixelToCoords(sf::Vector2i(event.mouseMove.x, event.mouseMove.y));
+            break;
+          }
+        default:;
+      }
     }
 
+  window.setView(view);
     window.clear();
     window.draw(data_points);
     window.display();
